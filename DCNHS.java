51339c75565781f5622e7ed3470973903a9f3808
Jan Salvador Sebastian <sebastian@mclinica.com>
2023-03-06 09:08:01 +0800

    Added method for sop

--- DCNHS.java
+++ DCNHS.java
@@ -1,5 +1,9 @@
 class DCNHS {
     public static void main(String [] args) {
+        sop();
+    }
+
+    static void sop(){
         System.out.print("Geiko Branch");
         System.out.print("Geiko Branch II");
     }
